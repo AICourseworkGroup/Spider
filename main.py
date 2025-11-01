cbe8be584d1e9ec483bc99104778076c460b5fc0
Zac <zacwstill@gmail.com>
2025-11-01 14:34:31 +0000

    FINALLY SOME PROGRESS

--- main.py
+++ main.py
@@ -1,21 +1,61 @@
-import numpy as np
+import random as rd
+import math
 from plot_spider_pose import plot_spider_pose
 
-if __name__ == '__main__':
-    # Example usage translated from the MATLAB file
-    a = np.deg2rad(0.0)
-    b = np.deg2rad(-45.0)
-    c = np.deg2rad(-40.0)
+def createTargerChromosone():
 
-    L1 = np.array([a, b, c])
-    L2 = np.array([a, b, c])
-    L3 = np.array([a, b, c])
-    L4 = np.array([a, b, c])
-    R4 = np.array([-a, b, c])
-    R3 = np.array([-a, b, c])
-    R2 = np.array([-a, b, c])
-    R1 = np.array([-a, b, c])
+    a = math.radians(0)
+    b = math.radians(-45)
+    c = math.radians(-30)
 
-    angles = np.concatenate([L1, L2, L3, L4, R4, R3, R2, R1])
 
-    plot_spider_pose(angles)
+    l1 = [a, b, c]
+    l2 = [a, b, c]
+    l3 = [a, b, c]
+    l4 = [a, b, c]
+    r4 = [-a, b, c]
+    r3 = [-a, b, c]
+    r2 = [-a, b, c]
+    r1 = [-a, b, c]
+
+    # return a flat Python list (24 floats) in radians
+    angles = l1 + l2 + l3 + l4 + r4 + r3 + r2 + r1
+    return angles
+
+
+def randDegGen():
+    angle = math.radians(rd.randint(-180, 180))
+    return angle
+    
+
+def createRandomPopulation():
+    populationSize = 30
+    population = []
+
+    for i in range(populationSize):
+        l1 = [randDegGen(), randDegGen(), randDegGen()]
+        l2 = [randDegGen(), randDegGen(), randDegGen()]
+        l3 = [randDegGen(), randDegGen(), randDegGen()]
+        l4 = [randDegGen(), randDegGen(), randDegGen()]
+        r4 = [randDegGen(), randDegGen(), randDegGen()]
+        r3 = [randDegGen(), randDegGen(), randDegGen()]
+        r2 = [randDegGen(), randDegGen(), randDegGen()]
+        r1 = [randDegGen(), randDegGen(), randDegGen()]
+
+        angles = l1 + l2 + l3 + l4 + r4 + r3 + r2 + r1
+        population.append(angles)
+
+    return population
+
+
+targetChromosone = createTargerChromosone()
+def calculateFitness(inputAngles, targetChromosone):
+    diffs = [abs(t - i) for t, i in zip(targetChromosone, inputAngles)]
+    fitness = sum(diffs)
+    return fitness
+
+population = createRandomPopulation()
+print(f'population: {population}')
+print(f'targetChromosone: {targetChromosone}')
+print(f'fitness of population[4]: {calculateFitness(population[4], targetChromosone)}')
+
