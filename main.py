d7c146161f169235fef476147a3c3987f669ea7d
Zac <zacwstill@gmail.com>
2025-11-01 13:07:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from plot_spider_pose import plot_spider_pose
 
 if __name__ == '__main__':
     # Example usage translated from the MATLAB file
-    T1_rad = np.deg2rad(0.0)
-    T2_rad = np.deg2rad(-45.0)
-    T3_rad = np.deg2rad(-40.0)
+    a = np.deg2rad(0.0)
+    b = np.deg2rad(-45.0)
+    c = np.deg2rad(-40.0)
 
-    L1 = np.array([T1_rad, T2_rad, T3_rad])
-    L2 = np.array([T1_rad, T2_rad, T3_rad])
-    L3 = np.array([T1_rad, T2_rad, T3_rad])
-    L4 = np.array([T1_rad, T2_rad, T3_rad])
-    R1 = np.array([-T1_rad, T2_rad, T3_rad])
-    R2 = np.array([-T1_rad, T2_rad, T3_rad])
-    R3 = np.array([-T1_rad, T2_rad, T3_rad])
-    R4 = np.array([-T1_rad, T2_rad, T3_rad])
+    L1 = np.array([a, b, c])
+    L2 = np.array([a, b, c])
+    L3 = np.array([a, b, c])
+    L4 = np.array([a, b, c])
+    R1 = np.array([-a, b, c])
+    R2 = np.array([-a, b, c])
+    R3 = np.array([-a, b, c])
+    R4 = np.array([-a, b, c])
 
     angles = np.concatenate([L1, L2, L3, L4, R1, R2, R3, R4])
 
